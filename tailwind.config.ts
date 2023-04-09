d13f268dd8302aa5b93f75f1fafbed7385631890
Lucas Viana <lv201122@gmail.com>
2023-04-08 23:01:05 -0300

    Add the tailwind.config.ts file with some sample colors configured

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import colors from 'tailwindcss/colors'
+
+import type { Config } from 'tailwindcss'
+
+export default <Partial<Config>>{
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.blue,
+        muted: colors.slate[500]
+      }
+    }
+  }
+}
