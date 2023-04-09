316084d4080ef752f3db23f0a9551cb3c6fc48a2
Lucas Viana <lv201122@gmail.com>
2023-04-08 21:03:26 -0300

    Add the ExperimentCard component

---
+++ components/shared/experiments/ExperimentCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="border rounded p-3 hover:border-blue-700 active:text-blue-700 active:bg-blue-100 transition-colors w-full">
+    {{ props.experiment.name }}
+  </button>
+</template>
+
+<script lang="ts" setup>
+import { Experiment } from '~/types/experiments'
+
+const props = defineProps<{
+  experiment: Experiment;
+}>()
+</script>
