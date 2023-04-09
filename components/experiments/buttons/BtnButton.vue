01d3ad95dd004688f179721964a8f2621e1a28df
Lucas Viana <lv201122@gmail.com>
2023-04-08 23:01:39 -0300

    Add initial version of the BtnButton component for experimentation

---
+++ components/experiments/buttons/BtnButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <button
+    class="btn-button px-4 py-2 rounded transition-all focus:ring-4"
+    :class="classes"
+  >
+    <slot />
+  </button>
+</template>
+
+<script lang="ts" setup>
+type ButtonVariant = 'primary' | 'secondary';
+
+const props = defineProps<{
+  variant: ButtonVariant;
+}>()
+
+const variantClasses: Record<ButtonVariant, string> = {
+  primary: 'bg-blue-500 text-blue-100 ring-blue-300 hover:bg-blue-600 active:bg-blue-700',
+  secondary: 'bg-gray-500 text-gray-100 ring-gray-300 hover:bg-gray-600 active:bg-gray-700'
+}
+
+const classes = computed(() => ([
+  '',
+  variantClasses[props.variant]
+]))
+</script>
+
+<style lang="scss" scoped>
+.btn-button {
+
+}
+</style>
--- pages/experiments/buttons/index.vue
+++ pages/experiments/buttons/index.vue
@@ -1,11 +1,21 @@
 <template>
   <NuxtLayout name="home">
-    <Container class="p-3">
-      <code>(WIP 🚧)</code>
+    <Container class="p-3 flex flex-col gap-3">
+      <p>The <code>primary</code> button:</p>
+      <ExperimentCanvas>
+        <BtnButton variant="primary">
+          Get Started
+        </BtnButton>
+        <template #caption>
+          Used to perform the main actions.
+        </template>
+      </ExperimentCanvas>
     </Container>
   </NuxtLayout>
 </template>
 
 <script lang="ts" setup>
 import Container from '~/components/shared/layout/Container.vue'
+import ExperimentCanvas from '~/components/shared/experiments/ExperimentCanvas.vue'
+import BtnButton from '~/components/experiments/buttons/BtnButton.vue'
 </script>
