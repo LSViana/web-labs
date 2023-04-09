8963241d292d4cae2aa44504d16eeb043d5a84d1
Lucas Viana <lv201122@gmail.com>
2023-04-09 11:33:59 -0300

    Add sample @click event definition to WlButton and experiment with it

--- components/experiments/buttons/WlButton.vue
+++ components/experiments/buttons/WlButton.vue
@@ -2,24 +2,35 @@
   <button
     class="btn-button px-4 py-2 rounded transition-[background-color,box-shadow] focus:ring-4"
     :class="classes"
+    @click="onClick"
   >
     <slot />
   </button>
 </template>
 
 <script lang="ts" setup>
-type ButtonVariant = 'primary' | 'secondary';
+import { computed } from 'vue'
 
-const props = defineProps<{
+type ButtonVariant = 'primary' | 'secondary'
+type Props = {
   variant: ButtonVariant;
-}>()
+}
+type Events = {
+  (e: 'click', event: MouseEvent): void;
+}
 
 const variantClasses: Record<ButtonVariant, string> = {
-  primary: 'bg-primary-500 text-primary-100 ring-primary-300 hover:bg-primary-600 active:bg-primary-700',
+  primary: 'bg-primary-500 text-primary-50 ring-primary-300 hover:bg-primary-600 active:bg-primary-700',
   secondary: 'bg-gray-500 text-gray-100 ring-gray-300 hover:bg-gray-600 active:bg-gray-700'
 }
 
+const props = defineProps<Props>()
+const emits = defineEmits<Events>()
 const classes = computed(() => ([
   variantClasses[props.variant]
 ]))
+
+function onClick (event: MouseEvent) {
+  emits('click', event)
+}
 </script>
--- pages/experiments/buttons/index.vue
+++ pages/experiments/buttons/index.vue
@@ -3,11 +3,11 @@
     <Container class="p-3 flex flex-col gap-3">
       <p>The <code>primary</code> button:</p>
       <ExperimentCanvas>
-        <WlButton variant="primary">
+        <WlButton variant="primary" @click="onClick">
           Get Started
         </WlButton>
         <template #caption>
-          Used to perform the main actions.
+          Used to perform the main actions. Clicked <code>{{ counter }}</code> times.
         </template>
       </ExperimentCanvas>
     </Container>
@@ -15,7 +15,15 @@
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue'
+
 import Container from '~/components/shared/layout/Container.vue'
 import ExperimentCanvas from '~/components/shared/experiments/ExperimentCanvas.vue'
 import WlButton from '~/components/experiments/buttons/WlButton.vue'
+
+const counter = ref(0)
+
+function onClick () {
+  counter.value++
+}
 </script>
