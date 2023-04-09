0de54d1ad00ae554f534a2881cedd8fa15333f22
Lucas Viana <lv201122@gmail.com>
2023-04-09 19:29:55 -0300

    Add WlConfirmDeleteButton to make a specific use case of WlConfirmButton reusable

---
+++ components/experiments/buttons/WlConfirmDeleteButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <WlConfirmButton
+    variant="danger"
+    class="flex items-center gap-2"
+    @confirm="methods.onConfirm"
+    @start-confirm="methods.onStartConfirm"
+    @stop-confirm="methods.onStopConfirm"
+  >
+    <WlTrashAnimatedIcon :progress="progress" />
+    <slot>
+      <span>Hold to Delete</span>
+    </slot>
+  </WlConfirmButton>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+import WlTrashAnimatedIcon from '~/components/experiments/icons/animated/WlTrashAnimatedIcon.vue'
+import WlConfirmButton from '~/components/experiments/buttons/WlConfirmButton.vue'
+
+type Events = {
+    (e: 'confirm'): void;
+};
+
+const emits = defineEmits<Events>()
+
+const progress = ref(0)
+
+const methods = {
+  onStartConfirm (): void {
+    progress.value = 0.5
+  },
+  onStopConfirm (): void {
+    progress.value = 0
+  },
+  onConfirm (): void {
+    progress.value = 1
+    emits('confirm')
+  }
+}
+</script>
