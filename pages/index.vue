9f48ac5f85aa5c01d22b092a6d24c4472b892fd6
Lucas Viana <lv201122@gmail.com>
2023-04-08 21:04:46 -0300

    Define a few initial experiments on the home page (to be added)

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,26 @@
 <template>
   <NuxtLayout name="home">
-    <code>(WIP 🚧)</code>
+    <Container>
+      <div class="p-3">
+        <p>Experiments on the <b>web platform</b>.</p>
+        <ul class="mt-3 grid gap-3 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+          <li v-for="experiment in experiments" :key="experiment.route">
+            <ExperimentCard :experiment="experiment" />
+          </li>
+        </ul>
+      </div>
+    </Container>
   </NuxtLayout>
 </template>
+
+<script lang="ts" setup>
+import { Experiment } from '~/types/experiments'
+
+import Container from '~/components/shared/layout/Container.vue'
+import ExperimentCard from '~/components/shared/experiments/ExperimentCard.vue'
+
+const experiments: Experiment[] = [
+  { name: 'Buttons', route: '/experiments/buttons' },
+  { name: 'Drag & Drop', route: '/experiments/drag-n-drop' }
+]
+</script>
