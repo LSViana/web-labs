595d500b7692b993a71eba4721ac46fbd0f4db24
Lucas Viana <lv201122@gmail.com>
2023-04-09 23:00:52 -0300

    Rename the progress experiment page and add WlProgressBarTile

---
+++ components/experiments/progress/WlProgressBarTile.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="inline-flex gap-0.5">
+    <div
+      v-for="i in props.max"
+      :key="i"
+      class="w-1.5 h-6 rounded-full"
+      :class="[ i <= value ? 'bg-green-500' : 'bg-green-900' ]"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+type Props = {
+  value: number;
+  max: number;
+};
+
+const props = defineProps<Props>()
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ pages/experiments/progress/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <NuxtLayout name="home">
+    <Container class="p-3 flex flex-col gap-3">
+      <p>The tiled progress bar:</p>
+      <ExperimentCanvas>
+        <div class="flex">
+          <WlProgressBarTile :value="progressTile" :max="10" />
+        </div>
+        <template #caption>
+          Used to show progress when the number of steps is known.
+        </template>
+      </ExperimentCanvas>
+    </Container>
+  </NuxtLayout>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+import Container from '~/components/shared/layout/Container.vue'
+import ExperimentCanvas from '~/components/shared/experiments/ExperimentCanvas.vue'
+import WlProgressBarTile from '~/components/experiments/progress/WlProgressBarTile.vue'
+
+const progressTile = ref(5)
+
+setInterval(() => {
+  progressTile.value = Math.min(Math.max(progressTile.value + (Math.random() > 0.5 ? -1 : 1), 0), 10)
+}, 500)
+</script>
