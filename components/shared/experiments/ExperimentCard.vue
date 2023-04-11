60f9e24163c07e108601e5dc08c95f9e5930e909
Lucas Viana <lv201122@gmail.com>
2023-04-10 23:17:42 -0300

    Fix colors on ExperimentCard to better match the style of the public area

--- components/shared/experiments/ExperimentCard.vue
+++ components/shared/experiments/ExperimentCard.vue
@@ -1,7 +1,9 @@
 <template>
   <NuxtLink
     class="
-      border rounded p-3 hover:border-blue-700 active:text-blue-700 active:bg-blue-100
+      border rounded p-3 hover:border-slate-400
+      active:bg-slate-300
+      dark:active:text-slate-50 dark:active:bg-slate-700
       transition-colors block text-center
     "
     :to="props.experiment.route"
