21a22c114bb42325d6f8efd008252c468d24b5ca
Lucas Viana <lv201122@gmail.com>
2023-04-10 21:44:46 -0300

    Add "Back to home" link on the shared Header component

--- components/shared/surroundings/Header.vue
+++ components/shared/surroundings/Header.vue
@@ -1,15 +1,24 @@
 <template>
   <header class="border-b">
-    <Container class="p-3">
+    <Container class="flex justify-between items-center">
       <h2 class="text-2xl">
-        <NuxtLink to="/">
+        <NuxtLink to="/" class="p-3 inline-block">
           Web Labs
         </NuxtLink>
       </h2>
+      <NuxtLink v-if="!isHome" to="/" class="inline-block p-3">
+        Back to home
+      </NuxtLink>
     </Container>
   </header>
 </template>
 
 <script setup>
+import { useRoute } from '#app'
+import { computed } from 'vue'
+
 import Container from '~/components/shared/layout/Container.vue'
+
+const route = useRoute()
+const isHome = computed(() => route.path === '/')
 </script>
