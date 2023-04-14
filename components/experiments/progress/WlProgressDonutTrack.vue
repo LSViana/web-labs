4c68cacc15f797b1c793da9cd618e64866c9ad0e
Lucas Viana <lv201122@gmail.com>
2023-04-14 19:38:39 -0300

    Introduce the WlProgressDonutTrack component

---
+++ components/experiments/progress/WlProgressDonutTrack.vue
@@ -0,0 +1,36 @@
+<template>
+  <svg width="100" height="100" class="-rotate-90">
+    <circle
+      cx="50%"
+      cy="50%"
+      r="50%"
+      stroke-width="0.5rem"
+      class="stroke-red-900 fill-transparent"
+    />
+    <circle
+      r="50%"
+      cx="50%"
+      cy="50%"
+      stroke-width="0.5rem"
+      :stroke-dasharray="`calc(3.141592653589793 * ${percentageValue}%) calc(2 * 3.141592653589793 * 50%)`"
+      stroke-linecap="round"
+      class="stroke-red-500 fill-transparent transition-[stroke-dasharray]"
+    />
+  </svg>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+type Props = {
+  value: number;
+  max: number;
+};
+
+const props = defineProps<Props>()
+
+const percentageValue = computed(() => props.value / props.max * 100)
+</script>
+
+<style lang="scss" scoped>
+</style>
