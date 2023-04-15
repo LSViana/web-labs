4dc921ab35c4d5426cf2f55c727b82364a3668ee
Lucas Viana <lv201122@gmail.com>
2023-04-15 11:25:38 -0300

    Introduce WlButtonGroup to put multiple actions together

---
+++ components/experiments/buttons/WlButtonGroup.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wl-button-group flex flex-nowrap">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wl-button-group {
+  & > :deep(*:not(:first-child):not(:last-child)) {
+    border-radius: 0;
+  }
+
+  & > :deep(*:first-child) {
+    border-bottom-right-radius: 0;
+    border-top-right-radius: 0;
+  }
+
+  & > :deep(*:last-child) {
+    border-bottom-left-radius: 0;
+    border-top-left-radius: 0;
+  }
+}
+</style>
