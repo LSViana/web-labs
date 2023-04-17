d63fb099920ae2d554c678c43745860013c846aa
Lucas Viana <lv201122@gmail.com>
2023-04-16 23:40:27 -0300

    Introduce WlRatingButton

---
+++ components/experiments/buttons/WlRatingButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <WlButtonGroup class="wl-rating-button inline-flex">
+    <WlButton v-for="i in 5" :key="i" :variant="props.variant" class="flex" @click="methods.onClick(i)">
+      <WlStarIcon :value="i <= props.value" />
+    </WlButton>
+  </WlButtonGroup>
+</template>
+
+<script lang="ts" setup>
+import WlStarIcon from '~/components/experiments/icons/static/WlStarIcon.vue'
+import WlButton, { WlButtonVariant } from '~/components/experiments/buttons/WlButton.vue'
+import WlButtonGroup from '~/components/experiments/buttons/WlButtonGroup.vue'
+
+type Props = {
+  value: number;
+  variant?: WlButtonVariant;
+};
+type Events = {
+  (e: 'update:value', event: number): void;
+};
+
+const props = withDefaults(defineProps<Props>(), {
+  variant: 'primary'
+})
+const emits = defineEmits<Events>()
+
+const methods = {
+  onClick (value: number): void {
+    emits('update:value', value)
+  }
+}
+</script>
