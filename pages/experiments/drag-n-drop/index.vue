3845f7d3ea7695ad9d601e1108ba0a8e4de9cd09
Lucas Viana <lv201122@gmail.com>
2023-04-17 23:32:24 -0300

    Introduce the experiment for WlSwitchDragAndDrop

--- pages/experiments/drag-n-drop/index.vue
+++ pages/experiments/drag-n-drop/index.vue
@@ -1,11 +1,23 @@
 <template>
   <NuxtLayout name="home">
-    <Container class="p-3">
-      <code>(WIP 🚧)</code>
+    <Container class="p-3 flex flex-col gap-3">
+      <p>The component used to toggle between mutually exclusive states:</p>
+      <ExperimentCanvas>
+        <WlSwitchDragAndDrop v-model:value="switchValue" :options="5" />
+        <template #caption>
+          Used to enable/disable something.
+        </template>
+      </ExperimentCanvas>
     </Container>
   </NuxtLayout>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue'
+
 import Container from '~/components/shared/layout/Container.vue'
+import ExperimentCanvas from '~/components/shared/experiments/ExperimentCanvas.vue'
+import WlSwitchDragAndDrop from '~/components/experiments/forms/switch/WlSwitchDragAndDrop.vue'
+
+const switchValue = ref(1)
 </script>
