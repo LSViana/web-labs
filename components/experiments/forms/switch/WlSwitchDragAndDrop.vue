7c57903a339750bdc36913222c8dd984e0904d8d
Lucas Viana <lv201122@gmail.com>
2023-04-18 09:53:30 -0300

    Improve styles for WlSwitchDragAndDrop

--- components/experiments/forms/switch/WlSwitchDragAndDrop.vue
+++ components/experiments/forms/switch/WlSwitchDragAndDrop.vue
@@ -3,15 +3,19 @@
     <div
       v-for="i in props.options"
       :key="i"
-      class="w-12 h-12 flex items-center justify-center rounded"
-      :class="[ backgroundColors[(i - 1) % backgroundColors.length] ]"
+      class="w-9 h-9 flex items-center justify-center rounded-full border-2 transition-colors"
+      :class="[
+        backgroundColors[(i - 1) % backgroundColors.length],
+        props.value === i ? borderColors[(i - 1) % borderColors.length] : 'border-transparent'
+      ]"
       :data-value="i"
       @dragover="listeners.dragover"
       @drop="listeners.drop"
+      @click="listeners.click"
     >
       <div
         v-if="props.value === i"
-        class="w-8 h-8 rounded"
+        class="w-6 h-6 rounded-full"
         :class="[ foregroundColors[(i - 1) % foregroundColors.length] ]"
         draggable="true"
         @dragstart="listeners.dragstart"
@@ -36,9 +40,14 @@ const props = defineProps<Props>()
 const emits = defineEmits<Events>()
 
 const backgroundColors = ['bg-green-900', 'bg-blue-900', 'bg-yellow-900', 'bg-red-900', 'bg-purple-900']
+const borderColors = ['border-green-500', 'border-blue-500', 'border-yellow-500', 'border-red-500', 'border-purple-500']
 const foregroundColors = ['bg-green-500', 'bg-blue-500', 'bg-yellow-500', 'bg-red-500', 'bg-purple-500']
 
 const listeners = {
+  click (event: MouseEvent): void {
+    const newValue = Number.parseInt((event.target as HTMLElement).dataset.value ?? '0')
+    methods.onInput(newValue)
+  },
   dragstart (event: DragEvent): void {
     if (!event.dataTransfer) {
       return
