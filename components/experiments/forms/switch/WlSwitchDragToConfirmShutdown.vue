cabec2df1c66719621cb45c985bd1daacaa76a1c
Lucas Viana <lv201122@gmail.com>
2023-04-23 20:13:39 -0300

    Introduce WlSwitchDragToConfirmShutdown

---
+++ components/experiments/forms/switch/WlSwitchDragToConfirmShutdown.vue
@@ -0,0 +1,22 @@
+<template>
+  <WlSwitchDragToConfirm @confirm="methods.onConfirm">
+    <WlPowerOffIcon class="text-slate-600 dark:text-slate-300" />
+  </WlSwitchDragToConfirm>
+</template>
+
+<script lang="ts" setup>
+import WlSwitchDragToConfirm from '~/components/experiments/forms/switch/WlSwitchDragToConfirm.vue'
+import WlPowerOffIcon from '~/components/experiments/icons/static/WlPowerOffIcon.vue'
+
+type Events = {
+  (e: 'confirm'): void;
+};
+
+const emits = defineEmits<Events>()
+
+const methods = {
+  onConfirm (): void {
+    emits('confirm')
+  }
+}
+</script>
