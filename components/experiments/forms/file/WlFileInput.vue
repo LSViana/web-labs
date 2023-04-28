b2d459ccfb1e4468607439c917d4ffce6fc0c57b
Lucas Viana <lv201122@gmail.com>
2023-04-27 23:48:58 -0300

    Add initial version of WlFileInput

---
+++ components/experiments/forms/file/WlFileInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <code>(WIP) </code>
+    <input type="file">
+  </div>
+</template>
+
+<script lang="ts" setup>
+type Props = {};
+type Events = {};
+
+const props = defineProps<Props>()
+const emits = defineEmits<Events>()
+
+const listeners = {}
+
+const methods = {}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- pages/experiments/drag-n-drop/index.vue
+++ pages/experiments/drag-n-drop/index.vue
@@ -15,6 +15,13 @@
           Used to confirm important actions. Confirmed: <code>{{ confirmed }}</code>.
         </template>
       </ExperimentCanvas>
+      <p>The component used to receive file uploads:</p>
+      <ExperimentCanvas>
+        <WlFileInput />
+        <template #caption>
+          Used to allow the user to upload files.
+        </template>
+      </ExperimentCanvas>
     </Container>
   </NuxtLayout>
 </template>
@@ -26,6 +33,7 @@ import Container from '~/components/shared/layout/Container.vue'
 import ExperimentCanvas from '~/components/shared/experiments/ExperimentCanvas.vue'
 import WlSwitchDragAndDrop from '~/components/experiments/forms/switch/WlSwitchDragAndDrop.vue'
 import WlSwitchDragToConfirmShutdown from '~/components/experiments/forms/switch/WlSwitchDragToConfirmShutdown.vue'
+import WlFileInput from '~/components/experiments/forms/file/WlFileInput.vue'
 
 const switchValue = ref(1)
 const confirmed = ref(false)
