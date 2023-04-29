33d3fe7b1914d017faecadeae17b2133831db829
Lucas Viana <lv201122@gmail.com>
2023-04-29 11:29:29 -0300

    Add sample MathStore class with tests for the add() method

---
+++ stores/math.ts
@@ -0,0 +1,5 @@
+export class MathStore {
+  add (x: number, y: number): number {
+    return x + y
+  }
+}
---
+++ tests/unit/stores/math.spec.ts
@@ -0,0 +1,21 @@
+import { describe, test, expect } from 'vitest'
+
+import { MathStore } from '~/stores/math'
+
+describe('Math Store', () => {
+  const store = new MathStore()
+
+  describe('add', () => {
+    test('two positive numbers', () => expect(store.add(1, 1)).toBe(2))
+
+    test('two negative numbers', () => expect(store.add(-1, -1)).toBe(-2))
+
+    test('a positive and a negative number', () => expect(store.add(1, -1)).toBe(0))
+
+    test('a number to zero', () => expect(store.add(1, 0)).toBe(1))
+
+    test('zero to a number', () => expect(store.add(0, 1)).toBe(1))
+
+    test('zero to zero', () => expect(store.add(0, 0)).toBe(0))
+  })
+})
