03e900c97e623a14ef826b1da31383bb8fe17203
Lucas Viana <lv201122@gmail.com>
2023-04-29 11:29:39 -0300

    Add sample tests for the WlButton component

---
+++ tests/unit/components/experiments/buttons/WlButton.spec.ts
@@ -0,0 +1,29 @@
+import { describe, expect, test } from 'vitest'
+import { mount } from '@vue/test-utils'
+
+import WlButton from '~/components/experiments/buttons/WlButton.vue'
+
+describe('WlButton', () => {
+  test('should render a <button>', () => {
+    const wrapper = mount(WlButton, {
+      props: {
+        variant: 'primary'
+      }
+    })
+
+    expect(wrapper.get('button').isVisible()).toBeTruthy()
+  })
+
+  test('should render the content of the default slot', () => {
+    const wrapper = mount(WlButton, {
+      props: {
+        variant: 'primary'
+      },
+      slots: {
+        default: 'Sign In'
+      }
+    })
+
+    expect(wrapper.text()).toBe('Sign In')
+  })
+})
