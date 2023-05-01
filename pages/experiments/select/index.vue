7f1e5a9e67d49b0e95f978838634b912d531f398
Lucas Viana <lv201122@gmail.com>
2023-05-01 00:53:14 -0300

    Add select experiment page

---
+++ pages/experiments/select/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <NuxtLayout name="home">
+    <Container class="p-3 flex flex-col gap-3">
+      <p>The tab select:</p>
+      <ExperimentCanvas>
+        <div class="flex">
+          <WlTabSelect v-model:value="tab" :options="options" />
+        </div>
+        <template #caption>
+          Used to select one of a <b>few</b> possible values.
+        </template>
+      </ExperimentCanvas>
+    </Container>
+  </NuxtLayout>
+</template>
+
+<script lang="ts" setup>
+import { computed, ref } from 'vue'
+
+import Container from '~/components/shared/layout/Container.vue'
+import ExperimentCanvas from '~/components/shared/experiments/ExperimentCanvas.vue'
+import WlTabSelect from '~/components/experiments/forms/select/WlTabSelect.vue'
+
+const tab = ref(0)
+const options = computed(() => ['Home', 'Features', 'Engineering', 'Pricing'])
+</script>
