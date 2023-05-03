4a9e76cd9165265ad021ad0e9c4a4344528b45d5
Lucas Viana <lv201122@gmail.com>
2023-05-03 20:29:54 -0300

    Add WlNotificationBadge component

---
+++ components/experiments/badges/WlNotificationBadge.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="absolute -top-3 -right-3 w-6 h-6 rounded-full bg-red-600 text-white flex justify-center items-center
+    text-sm transition-transform transform ease-in-out duration-300"
+    :class="classes"
+  >
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+type Props = {
+  value: boolean;
+};
+
+const props = defineProps<Props>()
+
+const classes = computed(() => ({
+  'scale-0': !props.value
+}))
+</script>
