4358716a860c96446dd9e533718972d7f0d84245
Lucas Viana <lv201122@gmail.com>
2023-05-02 23:14:27 -0300

    Improve styles for WlSwitchDragToConfirm

--- components/experiments/forms/switch/WlSwitchDragToConfirm.vue
+++ components/experiments/forms/switch/WlSwitchDragToConfirm.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="w-48 relative border rounded-full px-7 py-1 bg-slate-100 dark:bg-slate-800">
     <div
-      class="h-12 w-12 rounded-full bg-slate-300 dark:bg-slate-600 touch-none flex items-center justify-center -translate-x-1/2 transition-[border-color,background-color]"
+      class="h-12 w-12 rounded-full bg-slate-300 dark:bg-slate-600 touch-none flex items-center justify-center
+      hover:border cursor-ew-resize -translate-x-1/2 transition-[border-color,background-color]"
       :class="handleClasses"
       :style="handleStyles"
       @pointerup="listeners.pointerup"
