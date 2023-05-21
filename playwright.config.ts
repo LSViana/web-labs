e786a0870c63ce78e17e8ffb65d939b4333ac7b9
Lucas Viana <lv201122@gmail.com>
2023-05-21 07:48:55 -0300

    Add explicit configuration to HTML reporter on Playwright to match values used in docker-compose.yaml

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,14 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['html', { open: 'never' }]
+    [
+      'html',
+      {
+        open: 'on-failure',
+        host: '0.0.0.0',
+        port: 9323
+      }
+    ]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
