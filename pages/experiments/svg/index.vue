771a13a7eb5423ea7b6fdb17f1b993429f8acfcf
Lucas Viana <lv201122@gmail.com>
2023-05-26 18:59:31 -0300

    Add content for the SVG experiment page

--- pages/experiments/svg/index.vue
+++ pages/experiments/svg/index.vue
@@ -1,11 +1,20 @@
 <template>
   <NuxtLayout name="home">
-    <Container class="p-3">
-      <code>(WIP 🚧)</code>
+    <Container class="flex flex-col gap-3 p-3">
+      <p>The animated SVG gradient tracing:</p>
+      <ExperimentCanvas>
+        <WlAnimatedSvgGradientTracing />
+        <template #caption>
+          Demonstration based on the SVG gradient tracing created by
+          <a href="https://rauno.me/craft/nextjs" class="underline" target="_blank">Vercel designers</a>.
+        </template>
+      </ExperimentCanvas>
     </Container>
   </NuxtLayout>
 </template>
 
 <script lang="ts" setup>
 import Container from '~/components/shared/layout/Container.vue'
+import ExperimentCanvas from '~/components/shared/experiments/ExperimentCanvas.vue'
+import WlAnimatedSvgGradientTracing from '~/components/experiments/svg/WlAnimatedSvgGradientTracing.vue'
 </script>
