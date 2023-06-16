9fc5ab7988f90594e01cb42497d1a7bdf2372b35
Lucas Viana <lv201122@gmail.com>
2023-06-16 18:51:48 -0300

    Add experiment with Web Animations API to move element

--- pages/experiments/transitions/index.vue
+++ pages/experiments/transitions/index.vue
@@ -9,6 +9,10 @@
           </WlButton>
         </NuxtLink>
       </ExperimentCanvas>
+      <p>Animating an element to wherever the user clicks using Web Animations API:</p>
+      <ExperimentCanvas>
+        <WlMovableElementWebAnimationsApi />
+      </ExperimentCanvas>
     </Container>
   </NuxtLayout>
 </template>
@@ -17,6 +21,7 @@
 import Container from '~/components/shared/layout/Container.vue'
 import ExperimentCanvas from '~/components/shared/experiments/ExperimentCanvas.vue'
 import WlButton from '~/components/experiments/buttons/WlButton.vue'
+import WlMovableElementWebAnimationsApi from '~/components/experiments/transitions/WlMovableElementWebAnimationsApi.vue'
 
 import { useCustomViewTransitions } from '~/composables/view-transitions'
 
