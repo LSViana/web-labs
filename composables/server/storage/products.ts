8838f2804cbbb03cc5ef6d31b33319c0e10762a3
Lucas Viana <lv201122@gmail.com>
2023-07-02 08:34:18 -0300

    Add useProductsStorage() composable

---
+++ composables/server/storage/products.ts
@@ -0,0 +1,27 @@
+import { reactive } from 'vue'
+
+import { Product } from '~/utils/models/products'
+
+type ProductsStorage = {
+  products: Product[];
+  loadProducts: () => Promise<void>;
+}
+
+export function useProductsStorage (): ProductsStorage {
+  const products = reactive<Product[]>([])
+
+  const loadProducts = async (): Promise<void> => {
+    await new Promise(resolve => setTimeout(resolve, 1000))
+
+    products.push(
+      new Product('Milk'),
+      new Product('Banana'),
+      new Product('Cookies')
+    )
+  }
+
+  return {
+    products,
+    loadProducts
+  }
+}
