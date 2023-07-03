f6a685ff46d7ace3ff32b1d21cf7cc9cf1404553
Lucas Viana <lv201122@gmail.com>
2023-07-03 00:00:54 -0300

    Add the Google user info endpoint

---
+++ server/api/auth/google/user-info.ts
@@ -0,0 +1,19 @@
+import { defineEventHandler, getCookie, send, setResponseStatus } from 'h3'
+
+export default defineEventHandler(async (event) => {
+  const accessToken = getCookie(event, 'google_auth_access_token')
+
+  if (!accessToken) {
+    setResponseStatus(event, 401)
+
+    return await send(event)
+  }
+
+  const response = await fetch('https://www.googleapis.com/userinfo/v2/me', {
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
+  })
+
+  return await response.json()
+})
