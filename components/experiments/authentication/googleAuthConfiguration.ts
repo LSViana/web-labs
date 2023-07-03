949064b8a733cb489cf036032f0062c9fb3fb272
Lucas Viana <lv201122@gmail.com>
2023-07-03 00:00:27 -0300

    Add Google auth configuration file

---
+++ components/experiments/authentication/googleAuthConfiguration.ts
@@ -0,0 +1,8 @@
+const configuration = {
+  loginEndpoint: process.env.OAUTH_GOOGLE_LOGIN_ENDPOINT!,
+  tokenEndpoint: process.env.OAUTH_GOOGLE_TOKEN_ENDPOINT!,
+  clientId: process.env.OAUTH_GOOGLE_CLIENT_ID!,
+  clientSecret: process.env.OAUTH_GOOGLE_CLIENT_SECRET!
+}
+
+export default configuration
