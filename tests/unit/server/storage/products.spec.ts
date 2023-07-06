b606cc5b45b4a521263d0067e485b98aeac2b8ec
Lucas Viana <lv201122@gmail.com>
2023-07-06 00:15:28 -0300

    Add sample unit tests to the useProductsStorage() composable

---
+++ tests/unit/server/storage/products.spec.ts
@@ -0,0 +1,26 @@
+import { describe, expect, test, vi } from 'vitest'
+
+import { useProductsStorage } from '~/composables/server/storage/products'
+
+describe('Products Storage', () => {
+  vi.useFakeTimers()
+
+  test('starts empty', () => {
+    const store = useProductsStorage()
+
+    expect(store.products.length).toBe(0)
+  })
+
+  test('calling loadProducts fills the array', async () => {
+    const store = useProductsStorage()
+
+    expect(store.products.length).toBe(0)
+
+    await Promise.all([
+      vi.runAllTimersAsync(),
+      store.loadProducts()
+    ])
+
+    expect(store.products.length).toBe(3)
+  })
+})
