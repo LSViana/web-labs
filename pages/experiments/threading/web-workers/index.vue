33f5f803fe40bb7b6b7784a50fa593739972b0a3
Lucas Viana <lv201122@gmail.com>
2023-08-02 19:33:49 -0300

    Update the Web Workers sample to show the main thread blocked

--- pages/experiments/threading/web-workers/index.vue
+++ pages/experiments/threading/web-workers/index.vue
@@ -4,14 +4,14 @@
       <p>The single-threaded computation:</p>
       <ExperimentCanvas>
         <p>Result: <code class="inline-block w-[96px]">{{ singleThread.result }}</code> | Duration: <code>{{ singleThread.duration }}ms</code></p>
-        <WlButton variant="secondary" class="mt-2" @click="listeners.singleThreadClick">
+        <WlButton variant="secondary" class="mt-2 flex items-center gap-2" @click="listeners.singleThreadClick">
           Calculate
         </WlButton>
       </ExperimentCanvas>
       <p>The multi-threaded computation:</p>
       <ExperimentCanvas>
         <p>Result: <code class="inline-block w-[96px]">{{ multiThread.result }}</code> | Duration: <code>{{ multiThread.duration }}ms</code></p>
-        <WlButton variant="secondary" class="mt-2" @click="listeners.multiThreadClick">
+        <WlButton variant="secondary" class="mt-2 flex items-center gap-2" @click="listeners.multiThreadClick">
           Calculate
         </WlButton>
       </ExperimentCanvas>
@@ -42,11 +42,21 @@ const multiThread = reactive({
 
 const listeners = {
   singleThreadClick (): void {
+    // Reset the state
+    singleThread.result = 0
+    singleThread.duration = 0
+
+    // Calculate measuring performance
     const now = performance.now()
     singleThread.result = fibonacci.calculate(iterations)
     singleThread.duration = Math.round(performance.now() - now)
   },
   async multiThreadClick (): Promise<void> {
+    // Reset the state
+    multiThread.result = 0
+    multiThread.duration = 0
+
+    // Calculate measuring performance
     const now = performance.now()
     multiThread.result = await fibonacci.calculateAsync(iterations)
     multiThread.duration = Math.round(performance.now() - now)
