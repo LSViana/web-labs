c52865bd77863432eac8d3d7e9a11c9b3c67c3a5
Lucas Viana <lv201122@gmail.com>
2023-10-23 21:48:37 -0300

    Move the "Experiments" page from `/` under `/experiments`

---
+++ pages/experiments/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <NuxtLayout name="home">
+    <Container>
+      <div v-for="group in groups" :key="group.target" class="p-3">
+        <p>{{ group.target }}</p>
+        <ul class="mt-3 grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3">
+          <li v-for="experiment in group.experiments" :key="experiment.route">
+            <ExperimentCard :experiment="experiment" />
+          </li>
+        </ul>
+      </div>
+    </Container>
+  </NuxtLayout>
+</template>
+
+<script lang="ts" setup>
+import Container from '~/components/shared/layout/Container.vue'
+import ExperimentCard from '~/components/shared/experiments/ExperimentCard.vue'
+
+import type { Experiment } from '~/utils/types/experiments'
+
+type ExperimentGroup = {
+  target: string;
+  experiments: Experiment[];
+}
+
+const groups: ExperimentGroup[] = [
+  {
+    target: 'Forms & Input',
+    experiments: [
+      { name: 'Button', route: '/experiments/button' },
+      { name: 'Progress', route: '/experiments/progress' },
+      { name: 'Input', route: '/experiments/input' },
+      { name: 'Select', route: '/experiments/select' },
+      { name: 'Badge', route: '/experiments/badge' }
+    ]
+  },
+  {
+    target: 'Micro-interactions',
+    experiments: [
+      { name: 'Scroll', route: '/experiments/scroll' },
+      { name: 'SVG', route: '/experiments/svg' },
+      { name: 'Drag & Drop', route: '/experiments/drag-n-drop' }
+    ]
+  },
+  {
+    target: 'Navigation',
+    experiments: [
+      { name: 'Transitions', route: '/experiments/transitions' }
+    ]
+  },
+  {
+    target: 'Authentication',
+    experiments: [
+      { name: 'Third-party login', route: '/experiments/authentication/third-party-login' }
+    ]
+  },
+  {
+    target: 'Real-time Communication',
+    experiments: [
+      { name: 'Supabase', route: '/experiments/rtc/supabase' }
+    ]
+  },
+  {
+    target: 'Threading',
+    experiments: [
+      { name: 'Web Workers', route: '/experiments/threading/web-workers' }
+    ]
+  }
+]
+</script>
