07898702f3b1d56fa50b21142ca6bbbd098bfd29
Lucas Viana <lv201122@gmail.com>
2023-10-23 21:47:30 -0300

    Introduce the `WlCard` component

---
+++ components/experiments/card/WlCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <NuxtLink
+    class="
+      block rounded border p-3
+      text-center
+      transition-colors hover:border-slate-400
+      active:bg-slate-300 dark:active:bg-slate-700 dark:active:text-slate-50
+    "
+    :to="to"
+  >
+    <slot />
+  </NuxtLink>
+</template>
+
+<script lang="ts" setup>
+type Props = {
+  to: string;
+};
+type Events = {};
+
+const props = defineProps<Props>()
+const emits = defineEmits<Events>()
+
+const listeners = {}
+
+const methods = {}
+</script>
+
+<style lang="scss" scoped>
+</style>
