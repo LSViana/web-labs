392b104962051911b8bca049fc5d384ea6603cf0
Lucas Viana <lv201122@gmail.com>
2023-10-23 22:26:55 -0300

    Add the initial version of the `/applications` page

---
+++ pages/applications/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <NuxtLayout name="home">
+    <Container>
+      <div class="p-3">
+        <ul class="mt-3 grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3">
+          <li>
+            <WlCard>
+              <span>Chat</span>
+              <WlTodoBadge class="ml-2" />
+            </WlCard>
+          </li>
+          <li>
+            <WlCard>
+              <span>Servers</span>
+              <WlTodoBadge class="ml-2" />
+            </WlCard>
+          </li>
+          <li>
+            <WlCard>
+              <span>Notebooks</span>
+              <WlTodoBadge class="ml-2" />
+            </WlCard>
+          </li>
+          <li>
+            <WlCard>
+              <span>Papers</span>
+              <WlTodoBadge class="ml-2" />
+            </WlCard>
+          </li>
+          <li>
+            <WlCard>
+              <span>Posts</span>
+              <WlTodoBadge class="ml-2" />
+            </WlCard>
+          </li>
+          <li>
+            <WlCard>
+              <span>Colors</span>
+              <WlTodoBadge class="ml-2" />
+            </WlCard>
+          </li>
+          <li>
+            <WlCard>
+              <span>Inventory</span>
+              <WlTodoBadge class="ml-2" />
+            </WlCard>
+          </li>
+          <li>
+            <WlCard>
+              <span>Newsletter</span>
+              <WlTodoBadge class="ml-2" />
+            </WlCard>
+          </li>
+          <li>
+            <WlCard>
+              <span>Bookmarks</span>
+              <WlTodoBadge class="ml-2" />
+            </WlCard>
+          </li>
+          <li>
+            <WlCard>
+              <span>Budget</span>
+              <WlTodoBadge class="ml-2" />
+            </WlCard>
+          </li>
+        </ul>
+      </div>
+    </Container>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+import Container from '~/components/shared/layout/Container.vue'
+import WlCard from '~/components/experiments/card/WlCard.vue'
+import WlTodoBadge from '~/components/shared/badge/WlTodoBadge.vue'
+</script>
