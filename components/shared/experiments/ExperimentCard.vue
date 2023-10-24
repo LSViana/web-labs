ea00d154aad19e1664ced7101bfc1b7aa04df377
Lucas Viana <lv201122@gmail.com>
2023-10-23 21:47:40 -0300

    Refactor the `ExperimentCard` component to use the `WlCard`

--- components/shared/experiments/ExperimentCard.vue
+++ components/shared/experiments/ExperimentCard.vue
@@ -1,18 +1,12 @@
 <template>
-  <NuxtLink
-    class="
-      block rounded border p-3
-      text-center
-      transition-colors hover:border-slate-400
-      active:bg-slate-300 dark:active:bg-slate-700 dark:active:text-slate-50
-    "
-    :to="props.experiment.route"
-  >
+  <WlCard :to="props.experiment.route">
     {{ props.experiment.name }}
-  </NuxtLink>
+  </WlCard>
 </template>
 
 <script lang="ts" setup>
+import WlCard from '~/components/experiments/card/WlCard.vue'
+
 import type { Experiment } from '~/utils/types/experiments'
 
 const props = defineProps<{
