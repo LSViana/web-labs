8f027d77ae752937b600fcf370db7df0238a0f62
Lucas Viana <lv201122@gmail.com>
2023-10-24 08:13:28 -0300

    Drop the `keyframes` for the `bolt` animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,15 +15,6 @@ export default <Partial<Config>>{
           DEFAULT: colors.slate[500],
           dark: colors.slate[300]
         }
-      },
-      keyframes: {
-        bolt: {
-          from: { transform: 'translateX(-270px)' },
-          to: { transform: 'translateX(0)' }
-        }
-      },
-      animation: {
-        bolt: 'bolt 1s linear infinite'
       }
     }
   }
