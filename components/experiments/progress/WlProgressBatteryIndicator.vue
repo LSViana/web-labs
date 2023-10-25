71b6ec4516d237bd290319e734977ab43a8f40e9
Lucas Viana <lv201122@gmail.com>
2023-10-25 09:44:21 -0300

    Add support for showing the remaining time to charge

--- components/experiments/progress/WlProgressBatteryIndicator.vue
+++ components/experiments/progress/WlProgressBatteryIndicator.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="relative flex h-64 w-64 flex-col overflow-hidden rounded-3xl bg-gray-800 p-6">
-    <h2 class="text-5xl font-bold" :class="classes.text">
+    <h2 class="font text-5xl font-bold" :class="classes.text">
       {{ value }}%
     </h2>
     <p class="text-gray-400">
       {{ charging ? 'Charging...' : 'Charged' }}
+    </p>
+    <div class="grow" />
+    <p v-if="charging && value < 100" class="leading-5">
+      <span class="text-gray-200">{{ remainingTime }}</span>
+      <br>
+      <span class="text-gray-400">Until full charge</span>
     </p>
     <div class="absolute -bottom-16 -right-36">
       <svg
@@ -19,6 +25,7 @@
           <path d="M661 537H361L557.5 0L42.7688 644.914C25.5185 666.528 40.9076 698.5 68.5609 698.5H297.5L102 1230L661 537Z" class="fill-white" />
         </mask>
         <g mask="url(#bolt)">
+          <!-- d="M0 557L67.2437 583.696C109.788 600.586 157.188 600.525 199.688 583.525V583.525C242.262 566.495 289.921 566.532 332.517 583.507V583.507C375.108 600.479 423.193 600.431 465.828 583.568V583.568C508.688 566.616 556.85 566.468 599.688 583.475V583.475C642.6 600.512 690.4 600.512 733.312 583.475L800 557V1230H0V557Z" -->
           <path
             :d="batteryPath"
             class="origin-center transition-transform"
@@ -62,6 +69,22 @@ const classes = computed(() => {
       illustration: 'fill-blue-400'
     }
   }
+})
+
+const remainingMinutes = computed(() => (100 - props.value) * 180 / 100)
+const remainingTime = computed(() => {
+  const hours = Math.floor(remainingMinutes.value / 60)
+  const minutes = Math.round(remainingMinutes.value % 60)
+
+  if (minutes === 0) {
+    return `${hours}h`
+  }
+
+  if (hours === 0) {
+    return `${minutes}m`
+  }
+
+  return `${hours}h ${minutes}m`
 })
 
 const batteryPath = computed(() => {
