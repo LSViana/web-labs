0134d77bf295cb7c1e9972cc4c428004e374ff22
Lucas Viana <lv201122@gmail.com>
2023-10-29 01:28:00 -0300

    Introduce new `bolt` animation to the Tailwind configuration file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,15 @@ export default <Partial<Config>>{
           DEFAULT: colors.slate[500],
           dark: colors.slate[300]
         }
+      },
+      keyframes: {
+        bolt: {
+          from: { transform: 'translateX(-270px)' },
+          to: { transform: 'translateX(0)' }
+        }
+      },
+      animation: {
+        bolt: 'bolt 1s linear infinite'
       }
     }
   }
