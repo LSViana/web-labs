eb55c8f0c211d2a19f6a82ce0561b5e532815555
Lucas Viana <lv201122@gmail.com>
2023-12-26 20:54:39 -0300

    Introduce the `WlIconButton`

---
+++ components/experiments/buttons/WlIconButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <WlButton :variant="variant" class="h-10 w-10 rounded-full">
+    <slot />
+  </WlButton>
+</template>
+
+<script lang="ts" setup>
+import WlButton, { type WlButtonVariant } from '~/components/experiments/buttons/WlButton.vue'
+
+type Props = {
+  variant: WlButtonVariant;
+}
+
+defineProps<Props>()
+</script>
