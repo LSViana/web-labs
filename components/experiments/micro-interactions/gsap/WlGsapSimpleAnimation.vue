13da4cfed8ad0aa7d268e734525c9f0709ab8ef1
Lucas Viana <lv201122@gmail.com>
2023-12-31 14:53:06 -0300

    Add GSAP sample animation experiment

---
+++ components/experiments/micro-interactions/gsap/WlGsapSimpleAnimation.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="h-48">
+    <div ref="box1" class="h-6 w-6 rounded border border-amber-400 bg-amber-500" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue'
+import gsap from 'gsap'
+
+const box1 = ref<HTMLElement>()
+
+onMounted(async () => {
+  await gsap.to(box1.value!, { duration: 2, x: 100 })
+  await gsap.to(box1.value!, { duration: 2, y: 100 })
+  await gsap.to(box1.value!, { duration: 2, rotate: 27 })
+  await gsap.to(box1.value!, { duration: 2, x: 0 })
+  await gsap.to(box1.value!, { duration: 2, x: 0, y: 0, rotate: 0 })
+})
+</script>
--- pages/experiments/micro-interactions/gsap/index.vue
+++ pages/experiments/micro-interactions/gsap/index.vue
@@ -1,11 +1,16 @@
 <template>
   <NuxtLayout name="home">
     <Container class="flex flex-col gap-3 p-3">
-      <code>🚧 WIP</code>
+      <p>A simple animation example:</p>
+      <ExperimentCanvas>
+        <WlGsapSimpleAnimation />
+      </ExperimentCanvas>
     </Container>
   </NuxtLayout>
 </template>
 
 <script setup lang="ts">
 import Container from '~/components/shared/layout/Container.vue'
+import ExperimentCanvas from '~/components/shared/experiments/ExperimentCanvas.vue'
+import WlGsapSimpleAnimation from '~/components/experiments/micro-interactions/gsap/WlGsapSimpleAnimation.vue'
 </script>
