9690e647e80d82f98ee12efbb25e4d4673b9dfbd
Lucas Viana <lv201122@gmail.com>
2023-12-31 11:10:08 -0300

    Add the `WlSvgGrainyGradient` component

---
+++ components/experiments/svg/WlSvgGrainyGradient.vue
@@ -0,0 +1,20 @@
+<template>
+  <div />
+</template>
+
+<style lang="scss" scoped>
+div {
+  width: 300px;
+  height: 300px;
+  filter: contrast(500%) brightness(150%);
+  background-image:
+      linear-gradient(to bottom right,
+        rgba(blue, 70%),
+        rgba(green, 70%),
+        rgba(yellow, 70%)
+      ),
+      url('/img/noise-filter.svg');
+  background-size: cover;
+  background-color: white;
+}
+</style>
