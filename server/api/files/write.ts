33f795b7df72cc145e2b92e6c33a64fdbcdd2118
Lucas Viana <lv201122@gmail.com>
2024-01-11 22:44:51 -0300

    Add sample API endpoint to write a sample file

---
+++ server/api/files/write.ts
@@ -0,0 +1,10 @@
+import * as fs from 'fs'
+
+import { defineEventHandler, send, setResponseStatus } from 'h3'
+
+export default defineEventHandler(async (event) => {
+  await fs.writeFile('server/api/files/name', 'Lucas Viana', () => {})
+
+  setResponseStatus(event, 200)
+  await send(event)
+})
