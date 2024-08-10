5803fcac7870b1646d163b25d05eba9e2838cf62
Lucas Viana <lv201122@gmail.com>
2024-08-10 12:45:03 -0300

    Configure TailwindCSS plugin to allow `wl-*` CSS classes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,9 +6,23 @@ import stylistic from '@stylistic/eslint-plugin'
 // To debug this configuration file, run: `npx @eslint/config-inspector`.
 export default withNuxt([
   ...tailwindCssPlugin.configs['flat/recommended'],
+
+  // Adds a set of default rules to format the code.
   {
     plugins: {
       '@stylistic': stylistic,
+    }
+  },
+
+  // TailwindCSS
+  {
+    rules: {
+      'tailwindcss/no-custom-classname': [
+        'error',
+        {
+          whitelist: [ 'wl\\-[-\\w]+' ]
+        }
+      ]
     }
   }
 ])
