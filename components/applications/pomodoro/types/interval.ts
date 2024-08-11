bf35471a9e10fd939a7d3038a600823ec7ae4a67
Lucas Viana <lv201122@gmail.com>
2024-08-11 15:20:03 -0300

    Introduce helper functions in the `Interval` type

--- components/applications/pomodoro/types/interval.ts
+++ components/applications/pomodoro/types/interval.ts
@@ -7,7 +7,21 @@ export class Interval {
     this.seconds = seconds
   }
 
+  public addToDate(date: Date): Date {
+    const totalSeconds = this.minutes * 60 + this.seconds
+    const totalMs = totalSeconds * 1_000
+
+    return new Date(date.getTime() + totalMs)
+  }
+
   public toString(): string {
     return `${this.minutes.toString().padStart(2, '0')}:${this.seconds.toString().padStart(2, '0')}`
+  }
+
+  public static fromDates(startDate: Date, endDate: Date): Interval {
+    const diffMs = endDate.getTime() - startDate.getTime()
+    const diffSeconds = diffMs / 1_000
+
+    return new Interval(Math.floor(diffSeconds / 60), Math.floor(diffSeconds % 60))
   }
 }
