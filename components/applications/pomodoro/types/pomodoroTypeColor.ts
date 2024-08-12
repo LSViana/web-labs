65dbae4d75f83d0d8320c100fa34d3190e6e9ba9
Lucas Viana <lv201122@gmail.com>
2024-08-11 22:26:54 -0300

    Introduce the pomodoro type color definitions

---
+++ components/applications/pomodoro/types/pomodoroTypeColor.ts
@@ -0,0 +1,30 @@
+import { PomodoroIntervalType } from '~/components/applications/pomodoro/types/pomodoroType'
+
+type PomodoroTypeColors = {
+  backgroundInteractive: string;
+  background: string;
+  stroke: string;
+}
+
+const workColors = {
+  background: 'bg-primary-500',
+  backgroundInteractive: 'bg-primary-500 text-primary-50 hover:bg-primary-500 hover:bg-primary-600 active:bg-primary-700 focus:ring-primary-300',
+  stroke: 'stroke-primary-500'
+}
+
+const breakColors = {
+  background: 'bg-green-600',
+  backgroundInteractive: 'bg-green-600 text-green-50 hover:bg-green-500 hover:bg-green-600 active:bg-green-700 focus:ring-green-300',
+  stroke: 'stroke-green-600'
+}
+
+export function getPomodoroTypeColor(type: PomodoroIntervalType): PomodoroTypeColors {
+  switch (type) {
+    case PomodoroIntervalType.work:
+      return workColors
+    case PomodoroIntervalType.break:
+      return breakColors
+    default:
+      throw Error(`Unable to calculate duration for ${type}.`)
+  }
+}
