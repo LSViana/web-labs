1cd6ab0a901ec55cd0208f447a6bbfb19a192a10
Lucas Viana <lv201122@gmail.com>
2024-08-11 22:59:57 -0300

    Introduce the `useLeaveConfirmation()` composable

---
+++ components/applications/pomodoro/useLeaveConfirmation.ts
@@ -0,0 +1,18 @@
+export function useLeaveConfirmation() {
+  function beforeUnload(event: BeforeUnloadEvent) {
+    event.preventDefault()
+  }
+
+  function hold() {
+    window.addEventListener('beforeunload', beforeUnload)
+  }
+
+  function release() {
+    window.removeEventListener('beforeunload', beforeUnload)
+  }
+
+  return {
+    hold,
+    release
+  }
+}
--- pages/applications/pomodoro/index.vue
+++ pages/applications/pomodoro/index.vue
@@ -15,15 +15,28 @@ import WlPomodoro from '~/components/applications/pomodoro/WlPomodoro.vue'
 import type { PomodoroInterval } from '~/components/applications/pomodoro/types/pomodoroInterval'
 import WlPomodoroOverview from '~/components/applications/pomodoro/WlPomodoroOverview.vue'
 import { usePomodoroRecorder } from '~/components/applications/pomodoro/usePomodoroRecorder'
+import { usePomodoroStorage } from '~/components/applications/pomodoro/usePomodoroStorage'
+import { onMounted } from 'vue'
+import { useLeaveConfirmation } from '~/components/applications/pomodoro/useLeaveConfirmation'
 
 const pomodoroRecorder = usePomodoroRecorder()
+const pomodoroStorage = usePomodoroStorage()
+const leaveConfirmation = useLeaveConfirmation()
+
+onMounted(() => {
+  // The records are loaded `onMounted` not to cause hydration mismatches.
+  pomodoroRecorder.load(pomodoroStorage.loadToday())
+})
 
 const listeners = {
   interval(interval: PomodoroInterval): void {
     pomodoroRecorder.save(interval)
+    pomodoroStorage.saveToday(pomodoroRecorder.records.value)
+    leaveConfirmation.release()
   },
   play(): void {
     pomodoroRecorder.record()
+    leaveConfirmation.hold()
   }
 }
 </script>
