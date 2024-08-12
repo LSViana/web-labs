b8a1a9755c1833ba18b90051575fa8dcc7444828
Lucas Viana <lv201122@gmail.com>
2024-08-11 22:29:44 -0300

    Apply the `WlPomodoroOverview` component and `usePomodoroRecorder()` composable

--- pages/applications/pomodoro/index.vue
+++ pages/applications/pomodoro/index.vue
@@ -1,8 +1,9 @@
 <template>
   <NuxtLayout name="home">
     <WlContainer>
-      <div class="inline-flex p-3">
-        <WlPomodoro/>
+      <div class="flex gap-3 p-3">
+        <WlPomodoro @interval="listeners.interval" @play="listeners.play"/>
+        <WlPomodoroOverview :records="pomodoroRecorder.records.value"/>
       </div>
     </WlContainer>
   </NuxtLayout>
@@ -11,4 +12,18 @@
 <script lang="ts" setup>
 import WlContainer from '~/components/shared/layout/WlContainer.vue'
 import WlPomodoro from '~/components/applications/pomodoro/WlPomodoro.vue'
+import type { PomodoroInterval } from '~/components/applications/pomodoro/types/pomodoroInterval'
+import WlPomodoroOverview from '~/components/applications/pomodoro/WlPomodoroOverview.vue'
+import { usePomodoroRecorder } from '~/components/applications/pomodoro/usePomodoroRecorder'
+
+const pomodoroRecorder = usePomodoroRecorder()
+
+const listeners = {
+  interval(interval: PomodoroInterval): void {
+    pomodoroRecorder.save(interval)
+  },
+  play(): void {
+    pomodoroRecorder.record()
+  }
+}
 </script>
