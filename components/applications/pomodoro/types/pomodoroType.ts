797f2536a3bb3bba794fc8579046ab3677a1c56c
Lucas Viana <lv201122@gmail.com>
2024-08-21 20:15:19 -0300

    Introduce the labels for `PomodoroIntervalType`

--- components/applications/pomodoro/types/pomodoroType.ts
+++ components/applications/pomodoro/types/pomodoroType.ts
@@ -2,3 +2,8 @@ export enum PomodoroIntervalType {
   work = 'work',
   break = 'break'
 }
+
+export const PomodoroIntervalTypeLabels: Record<PomodoroIntervalType, string> = {
+  [PomodoroIntervalType.work]: 'Work',
+  [PomodoroIntervalType.break]: 'Break'
+}
