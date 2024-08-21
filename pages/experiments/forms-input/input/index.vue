272e3310021c1ddedbbbee1937d019e057ad7f4b
Lucas Viana <lv201122@gmail.com>
2024-08-21 18:48:34 -0300

    Introduce the experiment for the time input and refactor other input-related experiments

--- pages/experiments/forms-input/input/index.vue
+++ pages/experiments/forms-input/input/index.vue
@@ -4,12 +4,9 @@
       <p>The password strength meter (input is just for decoration):</p>
       <WlExperimentCanvas>
         <div class="flex w-56 flex-col">
-          <input
-            v-model="password"
-            type="password"
-            class="rounded border bg-slate-200 px-3 py-2 outline-0 focus:border-slate-400 dark:bg-slate-800"
-          >
-          <WlPasswordStrength :value="password" />
+          <WlInput v-model="password" type="password">
+            <WlPasswordStrength :value="password"/>
+          </wlinput>
         </div>
         <template #caption>
           Used to demonstrate to the user the strength of their password. For this example, length is strength.
@@ -18,16 +15,17 @@
       <p>The number input with an animated display:</p>
       <WlExperimentCanvas>
         <div class="flex w-56 flex-col">
-          <input
-            v-model="amount"
-            type="number"
-            class="rounded border bg-slate-200 px-3 py-2 outline-0 focus:border-slate-400 dark:bg-slate-800"
-          >
+          <WlInput v-model="amount" type="number"/>
           <div class="mt-3 flex h-6 items-center gap-1">
             <small>US$</small>
-            <WlNumberDisplay :value="amount" />
+            <WlNumberDisplay :value="amount"/>
           </div>
         </div>
+      </WlExperimentCanvas>
+      <p>The time input:</p>
+      <WlExperimentCanvas>
+        <WlTimeInput v-model="time"/>
+        <p class="mt-3">Time: <code>{{ time.toLocaleString() }}</code></p>
       </WlExperimentCanvas>
     </WlContainer>
   </NuxtLayout>
@@ -40,7 +38,10 @@ import WlContainer from '~/components/shared/layout/WlContainer.vue'
 import WlExperimentCanvas from '~/components/shared/experiments/WlExperimentCanvas.vue'
 import WlPasswordStrength from '~/components/experiments/forms-input/input/WlPasswordStrength.vue'
 import WlNumberDisplay from '~/components/experiments/forms-input/input/WlNumberDisplay.vue'
+import WlInput from '~/components/experiments/forms-input/input/WlInput.vue'
+import WlTimeInput from '~/components/experiments/forms-input/input/WlTimeInput.vue'
 
 const password = ref('')
 const amount = ref(0)
+const time = ref(new Date())
 </script>
