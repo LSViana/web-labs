6dcb11c154909363f60bd8e71007e4be2372ef20
Lucas Viana <lv201122@gmail.com>
2024-08-21 18:48:07 -0300

    Introduce the `WlInput` component

---
+++ components/experiments/forms-input/input/WlInput.vue
@@ -0,0 +1,19 @@
+<template>
+  <input
+      v-model="model"
+      :type="props.type"
+      class="rounded border bg-slate-200 px-3 py-2 outline-0 focus:border-slate-400 dark:bg-slate-800"
+  >
+</template>
+
+<script lang="ts" setup>
+type Props = {
+  type?: 'text' | 'password' | 'number' | 'date' | 'time' | 'email';
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  type: 'text'
+})
+
+const model = defineModel<string | number | Date>()
+</script>
