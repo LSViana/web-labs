36ebc33042335a1f907246ed6bbb9add1ca4e4f2
Lucas Viana <lv201122@gmail.com>
2024-08-20 23:44:01 -0300

    Introduce props and controls to select date in `WlPomodoroOverview`

--- components/applications/pomodoro/WlPomodoroOverview.vue
+++ components/applications/pomodoro/WlPomodoroOverview.vue
@@ -9,6 +9,17 @@
         <span class="text-muted dark:text-muted-dark">Break</span>
         <p class="text-4xl font-bold">{{ records.break }}</p>
       </div>
+      <div class="flex items-end gap-3">
+        <div class="flex flex-col">
+          <span class="text-muted dark:text-muted-dark">Date</span>
+          <input
+              v-model="date"
+              type="date"
+              class="rounded border bg-slate-200 px-3 py-2 outline-0 focus:border-slate-400 dark:bg-slate-700"
+          >
+        </div>
+        <WlButton variant="secondary" @click="listeners.today">Today</WlButton>
+      </div>
     </div>
     <WlPomodoroOverviewTimeline v-if="props.records.length > 0" :records="props.records"/>
   </div>
@@ -20,12 +31,27 @@ import { PomodoroIntervalType } from '~/components/applications/pomodoro/types/p
 import { Interval } from '~/components/applications/pomodoro/types/interval'
 import type { PomodoroRecord } from '~/components/applications/pomodoro/types/pomodoroRecord'
 import WlPomodoroOverviewTimeline from '~/components/applications/pomodoro/WlPomodoroOverviewTimeline.vue'
+import WlButton from '~/components/experiments/forms-input/buttons/WlButton.vue'
+import { useToday } from '~/components/applications/pomodoro/useToday'
 
 type Props = {
   records: PomodoroRecord[];
 };
 
 const props = defineProps<Props>()
+const date = defineModel<Date>('date', {
+  required: true,
+  set: (value: Date | string) => {
+    if (value instanceof Date) {
+      return value
+    } else {
+      return new Date(value + 'T00:00:00')
+    }
+  },
+  get: (value) => value.toISOString().split('T')[0]
+})
+
+const today = useToday()
 
 const records = computed(() => {
   const workSeconds = props.records
@@ -42,4 +68,10 @@ const records = computed(() => {
 
   return result
 })
+
+const listeners = {
+  today(): void {
+    date.value = today.get()
+  }
+}
 </script>
