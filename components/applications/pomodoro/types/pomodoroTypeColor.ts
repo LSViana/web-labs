fd5d744478f8f255239ab80279834995ee9bc56c
Lucas Viana <lv201122@gmail.com>
2024-08-24 16:28:06 -0300

    Use darker tones for pomodoro type colors to improve accessibility

--- components/applications/pomodoro/types/pomodoroTypeColor.ts
+++ components/applications/pomodoro/types/pomodoroTypeColor.ts
@@ -8,15 +8,15 @@ type PomodoroTypeColors = {
 }
 
 const workColors = {
-  background: 'bg-primary-500',
-  backgroundInteractive: 'bg-primary-500 text-primary-50 hover:bg-primary-500 hover:bg-primary-600 active:bg-primary-700 focus:ring-primary-300',
+  background: 'bg-primary-600',
+  backgroundInteractive: 'bg-primary-600 text-primary-50 hover:bg-primary-700 active:bg-primary-800 focus:ring-primary-300',
   border: 'border border-primary-800',
-  stroke: 'stroke-primary-500'
+  stroke: 'stroke-primary-600'
 }
 
 const breakColors = {
   background: 'bg-green-600',
-  backgroundInteractive: 'bg-green-600 text-green-50 hover:bg-green-500 hover:bg-green-600 active:bg-green-700 focus:ring-green-300',
+  backgroundInteractive: 'bg-green-600 text-green-50 hover:bg-green-700 active:bg-green-800 focus:ring-green-300',
   border: 'border border-green-800',
   stroke: 'stroke-green-600'
 }
