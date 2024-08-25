1cc62529f99ff025e32b34c13ceeb0be6a5705e7
Lucas Viana <lv201122@gmail.com>
2024-08-25 10:46:32 -0300

    Fix bug on pomodoro recorder that was adding records out of the correct order

--- components/applications/pomodoro/usePomodoroRecorder.ts
+++ components/applications/pomodoro/usePomodoroRecorder.ts
@@ -25,15 +25,37 @@ export function usePomodoroRecorder() {
 
     const endDate = now.get()
 
-    records.value.push(new PomodoroRecord(
+    addRecord(new PomodoroRecord(
       startDate,
       endDate,
-      pomodoroInterval.type
+      type
     ))
   }
 
   function load(newRecords: PomodoroRecord[]) {
     records.value.splice(0, records.value.length, ...newRecords)
+  }
+
+  function addRecord(record: PomodoroRecord) {
+    let index = -1
+
+    if (records.value.length === 0) {
+      index = 0
+    } else if (records.value[0].startDate >= record.endDate) {
+      index = 0
+    } else {
+      const nextIndex = records.value.findLastIndex(x => x.endDate <= record.startDate)
+
+      if (nextIndex !== -1) {
+        // If there is a previous record, add the new record after it.
+        index = nextIndex + 1
+      } else {
+        // If there is no next record, add it to the end.
+        index = records.value.length - 1
+      }
+    }
+
+    records.value.splice(index, 0, record)
   }
 
   return {
