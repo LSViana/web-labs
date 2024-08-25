b52107c8860eb85ff7b6ba2759cd825716a2d956
Lucas Viana <lv201122@gmail.com>
2024-08-25 11:57:03 -0300

    Refactor `usePomodoroRecorder()` not to hold a collection of records in memory

--- components/applications/pomodoro/usePomodoroRecorder.ts
+++ components/applications/pomodoro/usePomodoroRecorder.ts
@@ -1,12 +1,9 @@
-import { ref } from 'vue'
-
 import { PomodoroRecord } from '~/components/applications/pomodoro/types/pomodoroRecord'
 import type { PomodoroIntervalType } from '~/components/applications/pomodoro/types/pomodoroType'
 import { useNow } from '~/components/applications/pomodoro/useNow'
 
 export function usePomodoroRecorder() {
   const now = useNow()
-  const records = ref<PomodoroRecord[]>([])
 
   let isRecording = false
   let startDate = now.get()
@@ -16,7 +13,7 @@ export function usePomodoroRecorder() {
     startDate = now.get()
   }
 
-  function save(type: PomodoroIntervalType) {
+  function capture(type: PomodoroIntervalType): PomodoroRecord | undefined {
     if (!isRecording) {
       return
     }
@@ -25,43 +22,11 @@ export function usePomodoroRecorder() {
 
     const endDate = now.get()
 
-    addRecord(new PomodoroRecord(
-      startDate,
-      endDate,
-      type
-    ))
-  }
-
-  function load(newRecords: PomodoroRecord[]) {
-    records.value.splice(0, records.value.length, ...newRecords)
-  }
-
-  function addRecord(record: PomodoroRecord) {
-    let index = -1
-
-    if (records.value.length === 0) {
-      index = 0
-    } else if (records.value[0].startDate >= record.endDate) {
-      index = 0
-    } else {
-      const nextIndex = records.value.findLastIndex(x => x.endDate <= record.startDate)
-
-      if (nextIndex !== -1) {
-        // If there is a previous record, add the new record after it.
-        index = nextIndex + 1
-      } else {
-        // If there is no next record, add it to the end.
-        index = records.value.length - 1
-      }
-    }
-
-    records.value.splice(index, 0, record)
+    return new PomodoroRecord(startDate, endDate, type)
   }
 
   return {
     record,
-    save,
-    load,
-    records
+    capture
   }
 }
