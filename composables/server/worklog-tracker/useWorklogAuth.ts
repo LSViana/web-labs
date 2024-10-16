74f050b3e905dd5ed45c9b8ca30ef9dcd0c5b18e
Lucas Viana <lv201122@gmail.com>
2024-10-15 22:18:15 -0300

    Increase the authentication duration from 8 hours to 1 week

--- composables/server/worklog-tracker/useWorklogAuth.ts
+++ composables/server/worklog-tracker/useWorklogAuth.ts
@@ -2,10 +2,12 @@ import { deleteCookie, getCookie, type H3Event, setResponseStatus } from 'h3'
 import { setCookie } from 'h3'
 
 const millisecondsPerHour = 1000 * 60 * 60
+const millisecondsPerDay = millisecondsPerHour * 24
+const millisecondsPerWeek = millisecondsPerDay * 7
 
-export function useWorklogAuth () {
-  function setCredentials (event: H3Event, credentialsId: string) {
-    const expires = new Date(Date.now() + (millisecondsPerHour * 8))
+export function useWorklogAuth() {
+  function setCredentials(event: H3Event, credentialsId: string) {
+    const expires = new Date(Date.now() + millisecondsPerWeek)
 
     setCookie(event, 'credentials_id', credentialsId, {
       httpOnly: true,
@@ -19,12 +21,12 @@ export function useWorklogAuth () {
     })
   }
 
-  function removeCredentials (event: H3Event) {
+  function removeCredentials(event: H3Event) {
     deleteCookie(event, 'credentials_id', { secure: true })
     deleteCookie(event, 'authenticated', { secure: true })
   }
 
-  function getCredentials (event: H3Event): string {
+  function getCredentials(event: H3Event): string {
     const credentialsId = getCookie(event, 'credentials_id')
 
     if (!credentialsId) {
