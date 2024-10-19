047fd3ead4d5fa0ce0402d66d2324a057b728a97
Lucas Viana <lv201122@gmail.com>
2024-10-18 22:47:53 -0300

    Introduce the authentication middleware on `worklog-tracker` route

--- pages/applications/worklog-tracker/index.vue
+++ pages/applications/worklog-tracker/index.vue
@@ -7,6 +7,19 @@
 </template>
 
 <script setup lang="ts">
+import { navigateTo } from '#app'
+import { definePageMeta } from '#imports'
+import { useWorklogAuth } from '~/components/applications/worklog-tracker/useWorklogAuth'
 import WlWorklogTracker from '~/components/applications/worklog-tracker/WlWorklogTracker.vue'
 import WlContainer from '~/components/shared/layout/WlContainer.vue'
+
+definePageMeta({
+  middleware: () => {
+    const auth = useWorklogAuth()
+
+    if (!auth.authenticated.value) {
+      return navigateTo('/applications/worklog-tracker/login')
+    }
+  }
+})
 </script>
