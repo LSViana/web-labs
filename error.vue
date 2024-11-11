85b6ed2039fbd047c1dec4aaa59a0b08beec9fc8
Lucas Viana <lv201122@gmail.com>
2024-11-10 21:49:07 -0300

    Introduce a custom error page

---
+++ error.vue
@@ -0,0 +1,27 @@
+<template>
+  <NuxtLayout name="home">
+    <WlContainer class="p-3">
+      <div class="flex items-start gap-3 rounded border border-warning-500 bg-amber-500/10 p-3">
+        <span class="pt-0.5">
+          <WlTriangleExclamationIcon/>
+        </span>
+        <div class="flex flex-col">
+          <p class="font-bold">{{ props.error.statusCode }}</p>
+          <p>{{ props.error.message }}</p>
+        </div>
+      </div>
+    </WlContainer>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+import WlTriangleExclamationIcon from '~/components/shared/icons/static/WlTriangleExclamationIcon.vue'
+import WlContainer from '~/components/shared/layout/WlContainer.vue'
+
+type Props = {
+  error: NuxtError
+}
+
+const props = defineProps<Props>()
+</script>
