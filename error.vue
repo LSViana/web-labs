1302293388eb2818cc75a7d4de8f785e41646ad9
Lucas Viana <lv201122@gmail.com>
2024-11-14 20:32:13 -0300

    Add option to "Go Home" when an error happens

--- error.vue
+++ error.vue
@@ -8,6 +8,9 @@
         <div class="flex flex-col">
           <p class="font-bold">{{ props.error.statusCode }}</p>
           <p>{{ props.error.message }}</p>
+          <div class="mt-3">
+            <WlButton variant="secondary" @click="listeners.home">Go Home</WlButton>
+          </div>
         </div>
       </div>
     </WlContainer>
@@ -15,7 +18,8 @@
 </template>
 
 <script setup lang="ts">
-import type { NuxtError } from '#app'
+import { clearError, type NuxtError } from '#app'
+import WlButton from '~/components/experiments/forms-input/buttons/WlButton.vue'
 import WlTriangleExclamationIcon from '~/components/shared/icons/static/WlTriangleExclamationIcon.vue'
 import WlContainer from '~/components/shared/layout/WlContainer.vue'
 
@@ -29,4 +33,13 @@ const props = defineProps<Props>()
 //
 // The instance returned here is the same as `props.error`.
 // const globalError = useError()
+
+const listeners = {
+  home () {
+    clearError({
+      redirect: '/'
+    })
+  }
+}
 </script>
+
