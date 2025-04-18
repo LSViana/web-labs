c387a2f4e66876ac74ff29da34b5ed3b7fa14d4e
Lucas Viana <lv201122@gmail.com>
2025-04-18 15:44:05 -0300

    Remove worklog authentication and storage composables and related API endpoints

--- server/api/worklog-tracker/worklogs/auth/index.delete.ts
+++
@@ -1,9 +0,0 @@
-import { defineEventHandler } from 'h3'
-
-import { useWorklogAuth } from '~/composables/server/worklog-tracker/useWorklogAuth'
-
-export default defineEventHandler(async (event) => {
-  const auth = useWorklogAuth()
-
-  auth.removeCredentials(event)
-})
