5de3d9ef08690af3ca1351f676d9780c27afe9dd
Lucas Viana <lv201122@gmail.com>
2025-04-18 16:23:55 -0300

    Refactor `eslint.config.mjs` to organize custom rules and improve readability

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,49 +8,68 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 export default withNuxt([
   ...tailwindCssPlugin.configs['flat/recommended'],
 
-  // Adds a set of default rules to format the code.
   {
+    name: 'custom/eslint/rules',
     plugins: {
-      '@stylistic': stylisticPlugin
+      '@stylistic': stylisticPlugin,
     },
     rules: {
-      '@stylistic/indent': ['error', 2]
-    }
+      '@stylistic/indent': ['error', 2],
+    },
   },
 
   // Configures the sort order of imports.
   {
+    name: 'custom/eslint-plugin/rules',
     plugins: {
-      'simple-import-sort': eslintPluginSimpleImportSort
+      'simple-import-sort': eslintPluginSimpleImportSort,
     },
     rules: {
       'simple-import-sort/imports': 'error',
-      'simple-import-sort/exports': 'error'
-    }
+      'simple-import-sort/exports': 'error',
+    },
   },
 
-  // TypeScript,
   {
+    name: 'custom/typescript/rules',
     rules: {
       // Disabled to allow defining one event per line with repeated types on components
       // like `WlButton`, which then allows for proper code completion in the IDE.
-      '@typescript-eslint/unified-signatures': 'off'
-    }
+      '@typescript-eslint/unified-signatures': 'off',
+    },
   },
 
-  // TailwindCSS
   {
+    name: 'custom/tailwindcss/rules',
     rules: {
       'tailwindcss/no-custom-classname': [
         'error',
         {
-          whitelist: ['wl\\-[-\\w]+']
-        }
-      ]
-    }
-  }
+          whitelist: ['wl\\-[-\\w]+'],
+        },
+      ],
+    },
+  },
+
+  {
+    name: 'custom/vue/rules',
+    files: ['**/*.vue'],
+    rules: {
+      'vue/max-attributes-per-line': [
+        'warn',
+        {
+          singleline: {
+            max: 3,
+          },
+          multiline: {
+            max: 1,
+          },
+        },
+      ],
+    },
+  },
 ])
   .override('nuxt/typescript/rules', {
     // Ignores the submodules (like the React project).
-    ignores: ['submodules/**']
+    ignores: ['submodules/**'],
   })
