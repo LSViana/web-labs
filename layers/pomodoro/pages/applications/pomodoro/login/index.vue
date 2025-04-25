57603be975c9da2df22c8a73dba8b6731f06600e
Lucas Viana <lv201122@gmail.com>
2025-04-24 21:28:23 -0300

    Add authentication middleware to Pomodoro and Worklog Tracker login pages

--- layers/pomodoro/pages/applications/pomodoro/login/index.vue
+++ layers/pomodoro/pages/applications/pomodoro/login/index.vue
@@ -7,6 +7,17 @@
 </template>
 
 <script setup lang="ts">
+import { definePageMeta, navigateTo, useProductivityAuth } from '#imports';
 import WlContainer from '~~/layers/base/components/layout/WlContainer.vue';
 import WlPomodoroAuthForm from '~~/layers/pomodoro/components/WlPomodoroAuthForm.vue';
+
+definePageMeta({
+  middleware: () => {
+    const auth = useProductivityAuth();
+
+    if (auth.authenticated.value) {
+      return navigateTo('/applications/pomodoro');
+    }
+  },
+});
 </script>
--- layers/worklog-tracker/pages/applications/worklog-tracker/login/index.vue
+++ layers/worklog-tracker/pages/applications/worklog-tracker/login/index.vue
@@ -7,6 +7,17 @@
 </template>
 
 <script setup lang="ts">
+import { definePageMeta, navigateTo, useProductivityAuth } from '#imports';
 import WlContainer from '~~/layers/base/components/layout/WlContainer.vue';
 import WlWorklogAuthForm from '~~/layers/worklog-tracker/components/WlWorklogAuthForm.vue';
+
+definePageMeta({
+  middleware: () => {
+    const auth = useProductivityAuth();
+
+    if (auth.authenticated.value) {
+      return navigateTo('/applications/worklog-tracker');
+    }
+  },
+});
 </script>
